31e1e8b0846ddcd8790202db7d1619263a881f02
kiwphi <120708663+kiwphi@users.noreply.github.com>
2023-02-25 20:23:00 +0200

    add screen-shake to quad-damage & thor's hammer

--- modules/Actors/Player/Player.js
+++ modules/Actors/Player/Player.js
@@ -3,7 +3,7 @@ import { BlueLaser } from '../../Lasers/Friendly/BlueLaser.js';
 import { PLAYERSPRITE } from '../../Assets/Player.js';
 import { Shield } from './Shield.js';
 import { Flame } from './Flame.js';
-import { getClosestEnemyTo, randomInRange } from '../../Logic/Helpers.js';
+import { getClosestEnemyTo, randomInRange, shakeScreen } from '../../Logic/Helpers.js';
 import { Rocket } from '../../Lasers/Friendly/Rocket.js';
 import { Seeker } from '../../Lasers/Friendly/Seeker.js';
 import { Clock } from '../../Drops/Clock.js';
@@ -67,6 +67,11 @@ export class Player {
             // Seekers
             if (game.enemies.enemiesOnScreen() && game.state.variables.seekers) {
                 game.bluelasers.add(new Seeker(this.x, this.y, getClosestEnemyTo(this)));
+            }
+
+            // Quad-Damage
+            if (game.state.variables.quaddamage || game.state.variables.thorshammer) {
+                shakeScreen(3, 0.25);
             }
 
             // Drones

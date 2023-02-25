11e46f74cc404cc4a00fb438e4c2cc5993001df7
kiwphi <120708663+kiwphi@users.noreply.github.com>
2023-02-25 16:38:33 +0200

    add FPS control

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 import { Game } from './modules/Game.js';
 import { CollisionDetection } from './modules/Logic/Motion/CollisionDetection.js';
 
+// FPS control
+const FPS = 60;
+const interval = 1000 / FPS;
+let then;
+
+// Initiate game variable & start game loop
 export const game = new Game();
 window.requestAnimationFrame(gameloop);
 
-export function gameloop() {
+// Game loop
+export function gameloop(timestamp) {
     if (!game.state.paused && !game.state.over) {
-        game.draw();
-        game.move();
-        game.refresh();
-        CollisionDetection.checkCollisions();
-        return window.requestAnimationFrame(gameloop);
+        // Set 'then' variable the first time
+        if (then === undefined) {
+            then = timestamp;
+        }
+
+        // Calculate time difference between frames
+        // and run game logic accordingly
+        const delta = timestamp - then;
+        window.requestAnimationFrame(gameloop);
+        if (delta > interval) {
+            game.draw();
+            game.move();
+            game.refresh();
+            CollisionDetection.checkCollisions();
+            then = timestamp - (delta % interval);
+            return;
+        }
     }
     if (game.state.over) {
         return game.scene.drawGameOver();

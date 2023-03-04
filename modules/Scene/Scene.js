0811907bed9286459b7a0650efc0db561e0d1477
kiwphi <120708663+kiwphi@users.noreply.github.com>
2023-03-04 23:24:02 +0200

    move item-description drawing function to scene-utils

--- modules/Scene/Scene.js
+++ modules/Scene/Scene.js
@@ -217,29 +217,8 @@ export class Scene {
     }
 
     drawPause() {
-        // PAUSE GLASS SPRITE
         this.ctx.drawImage(GLASSPAUSESPRITE, 360, 125);
-
-        // ITEMS DESCRIPTIONS
-        const numberOfRows = 4;
-        const verticalGap = 50;
-        const horizontalGap = 220;
-        const startingX = 75;
-        const startingY = 165;
-        let currentX = startingX;
-        let currentY = startingY;
-
-        for (let i = 0; i < game.itemcontroller.descriptions.length; i++) {
-            // For each new line, return X to its original left position
-            // and shift Y down according to verticalGap
-            if (i % numberOfRows === 0) {
-                x = startingX;
-                y += verticalGap;
-            }
-            // Draw then shift X right according to horizontalGap
-            this.ctx.drawImage(game.itemcontroller.descriptions[i], currentX, currentY);
-            currentX += horizontalGap;
-        }
+        SceneUtils.drawItemsDescriptions();
     }
 
     drawHud() {

e4790c8a5a43edcac33d99d91ae58cf5667a87e0
kiwphi <120708663+kiwphi@users.noreply.github.com>
2023-03-18 18:37:15 +0200

    improve shell travel-distance calculation mechanism

--- modules/Lasers/Friendly/Shell.js
+++ modules/Lasers/Friendly/Shell.js
@@ -4,17 +4,32 @@ import { SHELL0SPRITE, SHELL1SPRITE, SHELL2SPRITE, SHELL3SPRITE } from '../../As
 
 const SPRITE = [SHELL0SPRITE, SHELL1SPRITE, SHELL2SPRITE, SHELL3SPRITE];
 const SPEED = 20;
-const SPREAD = 15;
-const RANGE = 300; // in ms. shells will shatter after 'RANGE' ms
+
+const SPREAD = 15; // horizontal spread
+const TRAVELDISTANCE = 275; // distance in px from starting y-coordinate
 
 export class Shell extends BlueLaser {
     constructor() {
         super();
+
         this.speed = SPEED;
         this.sprite = SPRITE[randomInRange(0, SPRITE.length - 1)];
         this.direction = 270 + randomInRange(-SPREAD, +SPREAD);
 
-        setTimeout(() => this.shatter(), RANGE);
         this.alreadyLooped = true; // shells don't loop so this variable is overridden
+
+        // keep track of starting y-coordinate to measure travelled distance
+        this.startingY = this.y;
+    }
+
+    move() {
+        super.move();
+        if (this.hasReachedDistance()) {
+            this.shatter();
+        }
+    }
+
+    hasReachedDistance() {
+        return this.startingY - this.y > TRAVELDISTANCE;
     }
 }

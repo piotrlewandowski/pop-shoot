94a16fa2c8f19279c43c2bed8cafbdcf72343f32
kiwphi <120708663+kiwphi@users.noreply.github.com>
2023-03-18 14:49:33 +0200

    add shotgun shells range-limit

--- modules/Lasers/Friendly/Shell.js
+++ modules/Lasers/Friendly/Shell.js
@@ -1,4 +1,3 @@
-import { game } from '../../../app.js';
 import { BlueLaser } from './BlueLaser.js';
 import { randomInRange } from '../../Logic/Helpers.js';
 import { SHELL0SPRITE, SHELL1SPRITE, SHELL2SPRITE, SHELL3SPRITE } from '../../Assets/Lasers.js';
@@ -6,6 +5,7 @@ import { SHELL0SPRITE, SHELL1SPRITE, SHELL2SPRITE, SHELL3SPRITE } from '../../As
 const SPRITE = [SHELL0SPRITE, SHELL1SPRITE, SHELL2SPRITE, SHELL3SPRITE];
 const SPEED = 20;
 const SPREAD = 15;
+const RANGE = 300; // in ms. shells will shatter after 'RANGE' ms
 
 export class Shell extends BlueLaser {
     constructor() {
@@ -13,5 +13,6 @@ export class Shell extends BlueLaser {
         this.speed = SPEED;
         this.sprite = SPRITE[randomInRange(0, SPRITE.length - 1)];
         this.direction = 270 + randomInRange(-SPREAD, +SPREAD);
+        setTimeout(() => this.shatter(), RANGE);
     }
 }

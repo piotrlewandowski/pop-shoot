a384f5d89711b61fa18d00a68d28df44bd1ffc7d
kiwphi <120708663+kiwphi@users.noreply.github.com>
2023-03-19 12:29:28 +0200

    add dev script

---
+++ dev.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+session_name='POP'
+
+##### SERVERS SESSION
+tmux new-session -d -s $session_name
+
+# DOCKER
+tmux rename-window -t 0 'nginx'
+tmux send-keys -t $session_name:0 'docker-compose -f nginx.yaml up' C-m
+
+# CODE
+tmux new-window -t $session1:1 -n 'code'
+tmux send-keys -t $session1:1 'nvim' C-m
+
+# ATTACH
+tmux a

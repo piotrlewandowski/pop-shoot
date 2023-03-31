b08ef198292ad7c2cf2a0ab74d547ad42d27b509
kiwphi <120708663+kiwphi@users.noreply.github.com>
2023-03-31 14:55:26 +0200

    update dev-script: re-order windows

--- dev.sh
+++ dev.sh
@@ -5,13 +5,13 @@ session='POP'
 # create session
 tmux new-session -d -s $session
 
-# code
-tmux rename-window -t 0 'code'
-tmux send-keys -t $session:0 'nvim' C-m
-
 # docker
-tmux new-window -t $session:1 -n 'docker'
-tmux send-keys -t $session:1 'docker-compose -f nginx.yaml up' C-m
+tmux rename-window -t 0 'docker'
+tmux send-keys -t $session:0 'docker-compose -f nginx.yaml up' C-m
+
+# code
+tmux new-window -t $session:1 -n 'code'
+tmux send-keys -t $session:1 'nvim' C-m
 
 # attach
 tmux a

e7b2cfe306c7fc5c485c4cd6b78b91043a10548d
Charley Forey <49599721+CSEEEE@users.noreply.github.com>
2023-06-26 10:22:00 -0600

    Update Controls.js
    Added joystick and touchscreen controls

--- modules/Logic/Motion/Controls.js
+++ modules/Logic/Motion/Controls.js
@@ -3,12 +3,30 @@ import { CANVAS } from '../../Assets/Other.js';
 
 const PAUSEBUTTON = 'Space';
 
+const UP_ARROW = 'ArrowUp';
+const DOWN_ARROW = 'ArrowDown';
+const LEFT_ARROW = 'ArrowLeft';
+const RIGHT_ARROW = 'ArrowRight';
+const SHOOT_BUTTON = 'KeyS'; // Your actual key may vary
+const SLOWMO_BUTTON = 'KeyD'; // Your actual key may vary
+
 export class Controls {
     static addControls() {
-        this.addMouseMovement();
-        this.addMouseClicks();
+        if (this.isMobileDevice()) {
+            this.addTouchMovement();
+            this.addTouchClicks();
+        } else {
+            this.addMouseMovement();
+            this.addMouseClicks();
+            this.addJoystickControls();
+        }
         this.addPauseButton();
         this.disableRightClickMenu();
+    }
+
+    static isMobileDevice() {
+      const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+      return isMobile;
     }
 
     // EVENTS
@@ -40,12 +58,41 @@ export class Controls {
         });
     }
 
+    static addTouchMovement() {
+        CANVAS.addEventListener('touchmove', this._touchMovement);
+    }
+
+    static removeTouchMovement() {
+        CANVAS.removeEventListener('touchmove', this._touchMovement);
+    }
+
+    static addTouchClicks() {
+        CANVAS.addEventListener('touchstart', this._touchClicks);
+        CANVAS.addEventListener('touchend', this._touchClicksRelease);
+    }
+
+    static removeTouchClicks() {
+        CANVAS.removeEventListener('touchstart', this._touchClicks);
+        CANVAS.removeEventListener('touchend', this._touchClicksRelease);
+    }
+
+    static addJoystickControls() {
+        document.addEventListener('keydown', this._joystickMovement);
+        document.addEventListener('keyup', this._joystickButtonRelease);
+    }
+
+    static removeJoystickControls() {
+        document.removeEventListener('keydown', this._joystickMovement);
+        document.removeEventListener('keyup', this._joystickButtonRelease);
+    }
+
     // FUNCTIONS
 
     static _mouseMovement(evt) {
         game.player.x = evt.clientX > CANVAS.width ? CANVAS.width : evt.clientX;
         game.player.y = evt.clientY > CANVAS.height ? CANVAS.height : evt.clientY;
     }
+
     static _mouseClicks(evt) {
         // left
         if (evt.button === 0) {
@@ -75,6 +122,31 @@ export class Controls {
         }
     }
 
+    static _touchMovement(evt) {
+        const touch = evt.touches[0];
+        if (touch) {
+            const rect = CANVAS.getBoundingClientRect();
+            const scaleX = CANVAS.width / rect.width;
+            const scaleY = CANVAS.height / rect.height;
+            game.player.x = (touch.clientX - rect.left) * scaleX;
+            game.player.y = (touch.clientY - rect.top) * scaleY;
+        }
+    }
+
+    static _touchClicks(evt) {
+        evt.preventDefault();
+        if (!game.state.time) {
+            game.state.startGame();
+            CANVAS.requestFullscreen();
+        }
+        game.player.unsetShoot();
+        game.player.setShoot();
+    }
+
+    static _touchClicksRelease() {
+        game.player.unsetShoot();
+    }
+
     static _pauseButton(evt) {
         if (evt.code === PAUSEBUTTON) {
             if (!game.state.over) {
@@ -82,6 +154,43 @@ export class Controls {
             } else {
                 game.state.replay();
             }
+        }
+    }
+
+    static _joystickMovement(evt) {
+        switch (evt.code) {
+            case UP_ARROW:
+                game.player.y = Math.max(game.player.y - 1, 0);
+                break;
+            case DOWN_ARROW:
+                game.player.y = Math.min(game.player.y + 1, CANVAS.height);
+                break;
+            case LEFT_ARROW:
+                game.player.x = Math.max(game.player.x - 1, 0);
+                break;
+            case RIGHT_ARROW:
+                game.player.x = Math.min(game.player.x + 1, CANVAS.width);
+                break;
+            default:
+                break;
+        }
+    }
+
+    static _joystickButtonRelease(evt) {
+        switch (evt.code) {
+            case SHOOT_BUTTON:
+                if (!game.state.time) {
+                    game.state.startGame();
+                    CANVAS.requestFullscreen();
+                }
+                game.player.unsetShoot(); // used to prevent stuck to autoshooting by accident
+                game.player.setShoot();
+                break;
+            case SLOWMO_BUTTON:
+                game.state.toggleSlowmo();
+                break;
+            default:
+                break;
         }
     }
 }
